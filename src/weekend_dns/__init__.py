cfc8b3538f372a283fc6786962ebe6f91acbc08c
Joe Kerhin <37146185+jkerhin@users.noreply.github.com>
2023-11-25 19:18:04 -0500

    Adds build_query

--- src/weekend_dns/__init__.py
+++ src/weekend_dns/__init__.py
@@ -1,9 +1,14 @@
 """Implementing DNS in a weekend!"""
+import random
 import struct
 
 from .models import DNSHeader, DNSQuestion
 
 __version__ = "0.0.1"
+
+random.seed(1)  # TODO: Need a better place for this
+TYPE_A = 1
+CLASS_IN = 1
 
 
 def encode_dns_name(domain_name: str) -> bytes:
@@ -11,3 +16,12 @@ def encode_dns_name(domain_name: str) -> bytes:
     for part in domain_name.encode("ascii").split(b"."):
         encoded += struct.pack("!B", len(part)) + part
     return encoded + b"\x00"
+
+
+def build_query(domain_name: str, record_type) -> bytes:
+    name = encode_dns_name(domain_name=domain_name)
+    id = random.randint(0, 65535)
+    RECURSION_DESIRED = 1 << 8
+    header = DNSHeader(id=id, num_questions=1, flags=RECURSION_DESIRED)
+    question = DNSQuestion(name=name, type_=record_type, class_=CLASS_IN)
+    return header.to_bytes() + question.to_bytes()
--- tests/test_initial.py
+++ tests/test_initial.py
@@ -4,3 +4,11 @@ import weekend_dns
 def test_encode_dns_name():
     encoded = weekend_dns.encode_dns_name("google.com")
     assert encoded == b"\x06google\x03com\x00"
+
+
+def test_build_query():
+    query = weekend_dns.build_query("example.com", weekend_dns.TYPE_A)
+    assert (
+        query
+        == b"D\xcb\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x07example\x03com\x00\x00\x01\x00\x01"
+    )
